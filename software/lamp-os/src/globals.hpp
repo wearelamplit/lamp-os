6aec7f8c88fa11afb7f2ad7ad0278035563bc174
sabjorn <info@sabjorn.net>
2025-12-01 16:27:09 -0800

    remove ifdef in standard_lamp.hpp -- #pragma once does this already

--- software/lamp-os/src/globals.hpp
+++ software/lamp-os/src/globals.hpp
@@ -1,7 +1,9 @@
 #ifndef LAMP_GLOBALS_H
 #define LAMP_GLOBALS_H
 
-#define LAMP_SHADE_PIN 12
-#define LAMP_BASE_PIN 14
+#include <Adafruit_NeoPixel.h>
+
+constexpr int LAMP_SHADE_PIN=12;
+constexpr int LAMP_BASE_PIN=14;
 
 #endif
--- software/lamp-os/src/lamps/standard_lamp.hpp
+++ software/lamp-os/src/lamps/standard_lamp.hpp
@@ -1,6 +1,4 @@
 #pragma once
-#ifndef STANDARD_LAMP_H
-#define STANDARD_LAMP_H
 
 #define LAMP_SHADE_PIN 12
 #define LAMP_BASE_PIN 14
@@ -31,4 +29,3 @@ void handleStageMode();
  * Whole lamp changes from the configuration tool
  */
 void handleWebSocket();
-#endif
