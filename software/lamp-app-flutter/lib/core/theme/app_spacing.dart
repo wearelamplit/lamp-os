b15d63e8c3ec414fce5c208e89d9575d57ccbeff
Jerrett Taylor <jerrett@gmail.com>
2026-06-27 00:53:40 -0700

    feat(theme): spacing + radius tokens

---
+++ software/lamp-app-flutter/lib/core/theme/app_spacing.dart
@@ -0,0 +1,13 @@
+/// 4/8pt spacing scale + radius tokens. Replaces magic-number EdgeInsets.
+abstract class AppSpace {
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 12;
+  static const double lg = 16;
+  static const double xl = 24;
+  static const double xxl = 32;
+}
+
+abstract class AppRadius {
+  static const double card = 12;
+}
